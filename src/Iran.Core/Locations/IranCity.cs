c00b9e020df32478a19fc92ff7160c1625511d55
iman nemati <imun22@gmmail.com>
2021-01-02 11:06:01 +0330

    Initial commit

---
+++ src/Iran.Core/Locations/IranCity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iran.Core.Locations {
+
+    public class IranCity {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhonePreNumber { get; set; }
+        public string AmarCode { get; set; }
+        public int? ParentId { get; set; }
+        public int? ProvinceId { get; set; }
+        public string ProvinceName { get; set; }
+        public int? CityId { get; set; }
+        public string CityName { get; set; }
+        public int? CountyId { get; set; }
+        public string CountyName { get; set; }
+        public int? DistrictId { get; set; }
+        public string DistrictName { get; set; }
+        public int? RuralSectionId { get; set; }
+        public string RuralSectionName { get; set; }
+        public string Latitude { get; set; }
+        public string Longtiude { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Iran.Core/Locations/IranCityType.cs
@@ -0,0 +1,29 @@
+﻿namespace Iran.Core.Locations
+{
+    public enum IranCityType {
+        /// <summary>
+        /// استان
+        /// </summary>
+        Province = 0,
+        /// <summary>
+        /// شهر
+        /// </summary>
+        City = 1,
+        /// <summary>
+        /// شهرستان
+        /// </summary>
+        County = 2,
+        /// <summary>
+        /// بخش
+        /// </summary>
+        District = 3,
+        /// <summary>
+        /// دهستان
+        /// </summary>
+        RuralSection = 4,
+        /// <summary>
+        /// روستا
+        /// </summary>
+        Village = 5
+    }
+}
